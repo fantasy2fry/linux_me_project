a931220b551467d1e612158d47421a933237ce0f
Norbert Frydrysiak <fantasy22wfry@gmail.com>
2024-01-11 15:48:42 +0100

    git stats norbert update and about_ui small change

--- ui/about_ui.R
+++ ui/about_ui.R
@@ -34,11 +34,11 @@ aboutUI <- function(id) {
         "Tu można dać grafikę archa"
       ),
       box(
-        title = "",
+        title = "How have we gathered the data?",
         width = NULL,
         status = "primary",
         solidHeader = TRUE,
-        "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+        includeMarkdown(readLines("README.md"))
       )
     ),
     column(
